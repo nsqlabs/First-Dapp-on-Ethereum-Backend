efe8a300ebc7b5e40f8dca4afe9cd3c05b3cd426
Brian@NSQLabs <nsqlabs@gmail.com>
2022-07-07 13:34:27 -0300

    First Version Finished

---
+++ scripts/run.js
@@ -0,0 +1,34 @@
+const main = async () => {
+  const [owner, randomWalletAddress] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount = await waveContract.getTotalWaves();
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log("Total Waves: ", waveCount)
+
+  waveTxn = await waveContract.connect(randomWalletAddress).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log("Total Waves: ", waveCount)
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
